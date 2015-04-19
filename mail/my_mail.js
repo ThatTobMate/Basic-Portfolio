a45edbf7b237c3f3d3226edd7590962a8b97396a
ThatTobMate <thale.android@gmail.com>
2015-04-19 13:27:28 +0100

    mailfix

---
+++ mail/my_mail.js
@@ -0,0 +1,28 @@
+
+sendForm = function(){
+  event.preventDefault
+
+  dataHash = {
+    '{name}' : $('#name').val(),
+    '{email}' : $('#email').val(),
+    '{phone}' : $('#phone').val(),
+    '{message}' : $('#message').val()
+      }
+
+  $.ajax({
+    url: 'http://pooleapp.com/stash/347cefc5-78dc-4d85-b789-6f40e4a8e296/',
+    method: 'POST',
+    dataType: 'json',
+    data: dataHash
+  }).success(function(){
+    console.log('done')
+  })
+}
+
+
+$(function(){
+  $('form').on('submit', function(event){
+    event.preventDefault()
+    sendForm()
+  })
+})
