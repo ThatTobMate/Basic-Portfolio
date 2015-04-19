f663f96afb9e86039f6d97f6ef5e1b32927e088b
ThatTobMate <thale.android@gmail.com>
2015-04-19 13:44:42 +0100

    mailfinalfix

--- mail/my_mail.js
+++ mail/my_mail.js
@@ -16,7 +16,13 @@ sendForm = function(){
     data: dataHash
   }).always(function(){
     console.log('done')
-    $('#success > .alert-danger').append("<strong>Thank you "+ $('#name').val()+", your message has been sent. I will get back to you as soon as possible!");
+    $('#success').html("<div class='alert alert-success'>");
+    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+        .append("</button>");
+    $('#success > .alert-success')
+        .append("<strong>Thank you "+ $('#name').val()+", your message has been sent. I will get back to you as soon as possible!</strong>");
+    $('#success > .alert-success')
+        .append('</div>');
     $('form')[0].reset()
   })
 }
